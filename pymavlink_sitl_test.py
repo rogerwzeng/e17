46fe2acdc40d39eb1057ee86394338a574e2e8ed
Roger Zeng <roger@cora.org.cn>
2024-04-16 11:44:25 +0800

    More robust ACK_MSG management

--- pymavlink_sitl_test.py
+++ pymavlink_sitl_test.py
@@ -5,8 +5,7 @@ from pymavlink import mavutil
 
 # Connect to the Vehicle
 def connect_vehicle(connection_string):
-
-    if not connection_string :
+    if not connection_string:
         connection_string = 'udp:127.0.0.1:14551'  # default link port
 
     print('Connecting to vehicle on: %s' % connection_string)
@@ -18,22 +17,59 @@ def connect_vehicle(connection_string):
 
     return vehicle
 
-# Function to arm and take off
+
+# Clear any existing mission
+def clear_mission(vehicle):
+    vehicle.mav.mission_clear_all_send(vehicle.target_system, vehicle.target_component)
+    while True:
+        msg = vehicle.recv_match(type='MISSION_ACK', blocking=True)
+        if msg.type == mavutil.mavlink.MAV_MISSION_ACCEPTED:
+            print("Mission cleared")
+            break
+
+
+# Read in mission file and upload to drone
+def upload_mission(vehicle, filename):
+    with open(filename, 'r') as file:
+        first_line = file.readline()
+        if not first_line.startswith('QGC WPL 110'):
+            raise Exception('File format not supported')
+
+        mission_items = []
+        for line in file:
+            line = line.strip()
+            if line.startswith('#') or not line:
+                continue
+            parts = line.split('\t')
+            lat, lon, alt = map(float, parts[8:11])
+            seq = int(parts[0])
+            frame = int(parts[2])
+            command = int(parts[3])
+            mission_items.append(mavutil.mavlink.MAVLink_mission_item_message(
+                vehicle.target_system, vehicle.target_component, seq, frame,
+                command, 0, 0, 0, 0, 0, 0, lat, lon, alt))
+
+        vehicle.mav.mission_count_send(vehicle.target_system, vehicle.target_component, len(mission_items))
+
+        for i, item in enumerate(mission_items):
+            msg = vehicle.recv_match(type='MISSION_REQUEST', blocking=True)
+            vehicle.mav.send(item)
+
+        msg = vehicle.recv_match(type='MISSION_ACK', blocking=True)
+        if msg.type == mavutil.mavlink.MAV_MISSION_ACCEPTED:
+            print("Mission upload SUCCESS!")
+        else:
+            print("Mission upload FAILED!")
+
+        return len(mission_items)
+
+
+# Arm and take off the drone
 def arm_and_takeoff(vehicle, target_altitude):
-    guided_mode = vehicle.mode_mapping()["GUIDED"]
-    #print(f"Guided Mode is {guided_mode}")
-    takeoff_params = [0,0,0,0,0,0,target_altitude]
+    # Switch to GUIDED mode
+    set_mode(vehicle, "GUIDED")
 
-    vehicle.mav.command_long_send(
-        vehicle.target_system,
-        vehicle.target_component,
-        mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
-        mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
-        guided_mode, 0, 0, 0, 0, 0)
-    ack_msg = vehicle.recv_match(type='COMMAND_ACK', blocking=True, timeout=5)
-    print(f"Change mode to Guided: {ack_msg}")
-
-    # Confirm vehicle armed before attempting to take off
+    # Arm vehicle before attempting to take off
     print("Arming motors")
     vehicle.mav.command_long_send(
         vehicle.target_system,
@@ -60,84 +96,74 @@ def arm_and_takeoff(vehicle, target_altitude):
         altitude = msg.alt / 1000.0  # Convert from mm to m
 
         print("Altitude:", altitude)
-        if altitude >= target_altitude * 0.95:  # Just below target, in meters
+        if altitude > target_altitude * 0.95:  # Just below target, in meters
             print("Reached target altitude")
             break
         time.sleep(1)
 
-# Landing
-def land_vehicle(connection):
+
+# Land the vehicle
+def land_vehicle(vehicle):
     print("Landing")
-    connection.mav.command_long_send(
-        connection.target_system,
-        connection.target_component,
+    vehicle.mav.command_long_send(
+        vehicle.target_system,
+        vehicle.target_component,
         mavutil.mavlink.MAV_CMD_NAV_LAND,
         0, 0, 0, 0, 0, 0, 0, 0)
 
     print("Landing command sent")
 
-# Clear any existing mission
-def clear_mission(connection):
-    connection.mav.mission_clear_all_send(connection.target_system, connection.target_component)
-    while True:
-        msg = connection.recv_match(type='MISSION_ACK', blocking=True)
-        if msg.type == mavutil.mavlink.MAV_MISSION_ACCEPTED:
-            print("Mission cleared")
-            break
 
-def upload_mission(connection, filename):
-    with open(filename, 'r') as file:
-        first_line = file.readline()
-        if not first_line.startswith('QGC WPL 110'):
-            raise Exception('File format not supported')
+# Change vehicle mode
+def set_mode(vehicle, target_mode):
+    mode_id = vehicle.mode_mapping()[target_mode]
 
-        mission_items = []
-        for line in file:
-            line = line.strip()
-            if line.startswith('#') or not line:
-                continue
-            parts = line.split('\t')
-            lat, lon, alt = map(float, parts[8:11])
-            seq = int(parts[0])
-            frame = int(parts[2])
-            command = int(parts[3])
-            mission_items.append(mavutil.mavlink.MAVLink_mission_item_message(
-                connection.target_system, connection.target_component, seq, frame,
-                command, 0, 0, 0, 0, 0, 0, lat, lon, alt))
-
-        connection.mav.mission_count_send(connection.target_system, connection.target_component, len(mission_items))
-
-        for i, item in enumerate(mission_items):
-            msg = connection.recv_match(type='MISSION_REQUEST', blocking=True)
-            connection.mav.send(item)
-
-        msg = connection.recv_match(type='MISSION_ACK', blocking=True)
-        if msg.type == mavutil.mavlink.MAV_MISSION_ACCEPTED:
-            print("Mission upload SUCCESS!")
-        else:
-            print("Mission upload FAILED!")
-        
-        return len(mission_items)
-
-# Set the drone to ARMED AUTO mode
-def set_mode_auto(vehicle):
-    # Change mode to AUTO
-    auto_mode = vehicle.mode_mapping()["AUTO"]
-    print(f"Auto Mode is {auto_mode}")
-
+    # Send mode change command to AUTO mode
     vehicle.mav.command_long_send(
         vehicle.target_system,
         vehicle.target_component,
         mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
         mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
-        auto_mode, 0, 0, 0, 0, 0)
-    ack_msg = vehicle.recv_match(type='COMMAND_ACK', blocking=True, timeout=5)
+        mode_id, 0, 0, 0, 0, 0)
 
-    print("Mode set to AUTO")
+    if command_ack(vehicle, mavutil.mavlink.MAV_CMD_DO_SET_MODE):
+        print(f"Mode set to {target_mode}")
+    else:
+        print(f"FAILED to set mode to {target_mode}")
 
 
+# Check GPS fix before changing mode
+def check_gps_fix(vehicle):
+    ack_msg = vehicle.recv_match(type='GPS_RAW_INT', blocking=True)
+    if ack_msg.fix_type >= 3:  # Typically, a fix_type of 3 means a 3D fix
+        print("GPS Fix OK.")
+        return True
+    else:
+        print("Waiting for GPS Fix...")
+        return False
+
+
+# Check status message
+def command_ack(vehicle, cmd_id):
+    while True:
+        ack_msg = vehicle.recv_match(type='COMMAND_ACK', blocking=True, timeout=5)
+        if ack_msg is None:
+            print("Waiting for command acknowledgement...")
+            time.sleep(1)
+            continue
+        elif ack_msg.command == cmd_id:
+            if ack_msg.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
+                return True
+            else:
+                return False
+
+
+#################
+# START OF MAIN #
+#################
 # Protected main
 if __name__ == "__main__":
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description='commands')
     parser.add_argument('--connect')
     parser.add_argument('--mission')
@@ -149,13 +175,18 @@ if __name__ == "__main__":
 
     print(f"Connection String: {connection_string} ; Mission: {mission_file}")
 
-    # Mission workflow
+    # Mission Preparation
     copter = connect_vehicle(connection_string)
     clear_mission(copter)
     waypoints = upload_mission(copter, mission_file)
-    arm_and_takeoff(copter,20)  # initial height 20m
+
+    while not check_gps_fix(copter):
+        time.sleep(2)
+
+    # Mission Execution
+    arm_and_takeoff(copter, 20)  # initial height 20m
     time.sleep(5)  # let the copter catches its breath, before ..
-    set_mode_auto(copter)  # execute the mission
+    set_mode(copter, "AUTO")  # execute the mission
 
     # Monitor mission progress
     current_wp = 0
@@ -175,5 +206,3 @@ if __name__ == "__main__":
             print("Mission Completed")
             break
         time.sleep(5)
-
-
